83c7f999c0ef7cbec9537cd4681ff12ab94232a9
praveen_dung <praveen.dung@qualyval.com>
2025-03-28 18:59:53 +0530

    Add support for new line delimiters

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,8 +1,8 @@
 function add(numbers) {
     if (numbers === "") return 0;
 
-    const nums = numbers.split(",").map(Number);
-    return nums.reduce((sum,num)=>sum+num,0);
-  }
-  
-  module.exports = add;
+    const nums = numbers.split(/[\n,]/).map(Number);
+    return nums.reduce((sum, num) => sum + num, 0);
+}
+
+module.exports = add;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -15,3 +15,7 @@ test('returns sum of two numbers separated by comma', () => {
 test('returns sum of multiple numbers', () => {
     expect(add("1,2,3,4,5")).toBe(15);
 });
+
+test('handles new lines as delimiters', () => {
+    expect(add("1\n2,3")).toBe(6);
+});
