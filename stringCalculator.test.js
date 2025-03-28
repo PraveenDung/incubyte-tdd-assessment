3b5a05c372b092ad03286c38f00c6c0c4351ffc6
praveen_dung <praveen.dung@qualyval.com>
2025-03-28 19:07:25 +0530

    Add exception handling for negative numbers

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -23,3 +23,7 @@ test('handles new lines as delimiters', () => {
 test('supports custom delimiters', () => {
     expect(add("//;\n1;2")).toBe(3);
 });
+
+test('throws exception for negative numbers', () => {
+    expect(() => add("1,-2,3,-4")).toThrow("negative numbers not allowed: -2,-4");
+});
