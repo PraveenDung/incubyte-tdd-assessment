c1b7458c3ea265e882154d0e7b19fc9160827e23
praveen_dung <praveen.dung@qualyval.com>
2025-03-28 18:51:12 +0530

    Add test for single number input

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -3,3 +3,7 @@ const add = require('./stringCalculator');
 test('returns 0 for an empty string', () => {
   expect(add("")).toBe(0);
 });
+
+test('returns number itself for single input', () => {
+    expect(add("5")).toBe(5);
+  });
